48503a0f20702e802e2fca53774ffa085400059d
rrma1710 <rrma1710@gmail.com>
2022-12-27 08:57:27 +0700

    Test Insert

--- app/src/main/java/com/commit/crudroommvvm/EditActivity.kt
+++ app/src/main/java/com/commit/crudroommvvm/EditActivity.kt
@@ -1,11 +1,32 @@
 package com.commit.crudroommvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.commit.crudroommvvm.databinding.ActivityEditBinding
+import com.commit.crudroommvvm.room.Note
+import com.commit.crudroommvvm.room.NoteDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class EditActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditBinding
+    val db by lazy { NoteDB(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        binding = ActivityEditBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        binding.buttonSave.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                db.noteDao().addNote(
+                    Note(0, binding.editTitle.text.toString(), binding.editNote.text.toString())
+                )
+                finish()
+            }
+        }
     }
 }

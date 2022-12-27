ec70a771c42e5f6db984f47aafe194fe40d2fc9b
rrma1710 <rrma1710@gmail.com>
2022-12-27 09:03:59 +0700

    Add Response Log in MainActivity

--- app/src/main/java/com/commit/crudroommvvm/MainActivity.kt
+++ app/src/main/java/com/commit/crudroommvvm/MainActivity.kt
@@ -2,11 +2,18 @@ package com.commit.crudroommvvm
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.commit.crudroommvvm.databinding.ActivityMainBinding
+import com.commit.crudroommvvm.room.NoteDB
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    val db by lazy { NoteDB(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,14 @@ class MainActivity : AppCompatActivity() {
     private fun setupListener() {
         binding.buttonCreate.setOnClickListener {
             startActivity(Intent(this, EditActivity::class.java))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        CoroutineScope(Dispatchers.IO).launch {
+            val notes = db.noteDao().getNotes()
+            Log.d("MainActivity", "dbResponse : $notes")
         }
     }
 }
